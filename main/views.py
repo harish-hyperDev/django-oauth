f952f7773b4615913ecdf69130b70d9c2d3b5ba6
harish-hyperDev <coolprogramer.777@gmail.com>
2023-03-14 22:28:28 +0530

    added pages and tabs for different categories

--- main/views.py
+++ main/views.py
@@ -8,3 +8,14 @@ from django.shortcuts import render
 def login(response):
     pass
 
+def technology(response):
+    return render(response, "technology.html", {"visited": "technology"})
+
+def news(response):
+    return render(response, "news.html", {"visted": "news"})
+
+def sports(response):
+    return render(response, "sports.html", {"visted": "sports"})
+
+def life(response):
+    return render(response, "life.html", {"visted": "life"})
